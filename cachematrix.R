ef4cfdc8579de83e35820ab761ec0ec51ed1b1b9
kumar <kumarrams2001@yahoo.com>
2015-02-22 12:33:15 -0600

    Commit R course assignment 2
    Forked the cachematrix file from github and cloned it to desktop made my
    changes and committed it to my git repositiry

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These set of two functions help in storing a matrix and caching its inverse. The main purpose is to show 
+## the advantage of R scoping rules and how it helps in preserving the state in an R object
 
-## Write a short comment describing this function
+
+## This function sets the value, gets the value, sets the inverse 
+## and gets the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix with makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  
+  ## Checks if the inverse has already been calculated and if so gets it from the cache
+  
+  if(!is.null(m)) {
+    message("Getting Inverse Matrix")
+    return(m)
+  }
+  
+  ## If inverse has not been calculated calculate the inverse and the set the value
+  data <- x$get()
+  m <- solve(data,...)
+  x$setsolve(m)
+  m
 }
+
